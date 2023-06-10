ea1eeaefb846476d53149eb4d3af183f04e4fa6a
AnggaPutraa <yananggaa@gmail.com>
2023-06-10 23:49:58 +0700

    fix: body overflow hidden while modal open

--- app/components/core/organism/modals/_index.tsx
+++ app/components/core/organism/modals/_index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useCallback, useEffect, useState } from "react";
+import { MouseEventHandler, useCallback, useEffect, useState } from "react";
 import { IoMdClose } from "react-icons/io";
 import Button from "../../molecules/Button";
 
@@ -59,13 +59,28 @@ const Modal = ({
         secondaryAction();
     }, [secondaryAction, disabled]);
 
+    const handleCloseOnOutside: MouseEventHandler<HTMLDivElement> = useCallback((e) => {
+        if ((e.target as HTMLElement)?.id === 'base-modal') {
+            if (disabled) {
+                return;
+            }
+            setShowModal(false);
+            setTimeout(() => {
+                onClose();
+            }, 300);
+        }
+    }, [setShowModal, onClose]);
+
     if (!isOpen) {
         return null;
     }
 
     return (
         <>
-            <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
+            <div
+                id="base-modal"
+                onClick={handleCloseOnOutside}
+                className="flex justify-center items-center overflow-x-hidden overflow-y-hidden fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
                 <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto">
                     <div className={`translate duration-300 h-full ${showModal ? 'translate-y-0' : 'translate-y-full'} ${showModal ? 'opacity-100' : 'opacity-0'}`}>
                         <div className="translate h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
--- app/hooks/useLoginModal.ts
+++ app/hooks/useLoginModal.ts
@@ -8,8 +8,14 @@ interface LoginModalStore {
 
 const useLoginModal = create<LoginModalStore>((set) => ({
     isOpen: false,
-    onOpen: () => set({ isOpen: true }),
-    onClose: () => set({ isOpen: false }),
+    onOpen: () => {
+        set({ isOpen: true });
+        document.body.classList.add('overflow-hidden');
+    },
+    onClose: () => {
+        set({ isOpen: false });
+        document.body.classList.remove('overflow-hidden');
+    },
 }));
 
 export default useLoginModal;
