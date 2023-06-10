09742c642f4894727470bc12cf0ca0e653000492
AnggaPutraa <yananggaa@gmail.com>
2023-06-10 21:42:55 +0700

    refactor: add just textarea

--- app/components/core/molecules/TextArea.tsx
+++ app/components/core/molecules/TextArea.tsx
@@ -28,12 +28,20 @@ const TextArea = ({
                 id={id}
                 disabled={disabled}
                 {...register(id, { required })}
-                placeholder={placehoder}
-                className={`peer w-full p-4 h-[20vh] font-light bg-white border-2 rounded-md outline-none transition disabled:opacity-70 disabled:cursor-not-allowed
-                ${errors[id] ? 'border-rose-500' : 'border-neutral-300'}
-                ${errors[id] ? 'focus:border-rose-500' : 'focus:border-primary-500'}
-                `}
+                placeholder=" "
+                className={`peer w-full h-[20vh] p-4 pt-8 font-light bg-white border-2 rounded-md outline-none transition disabled:opacity-70 disabled:cursor-not-allowed pl-4
+            ${errors[id] ? 'border-rose-500' : 'border-neutral-300'}
+            ${errors[id] ? 'focus:border-rose-500' : 'focus:border-primary-500'}
+            `}
             />
+            <label
+                className={`absolute text-md duration-150 transform -translate-y-3 top-6 z-10 origin-[0] left-4
+        peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0
+        peer-focus:scale-75
+        peer-focus:-translate-y-4
+        ${errors[id] ? 'text-rose-500' : 'text-zinc-400'}`}>
+                {label}
+            </label>
         </div>
     );
 }
