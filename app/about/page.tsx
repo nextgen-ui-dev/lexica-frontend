79271ae6cb48ea72ccea345bdc025f6ad0848328
AnggaPutraa <yananggaa@gmail.com>
2023-06-30 01:06:18 +0800

    feat: about us

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,39 +1,19 @@
-import Image from "next/image";
-import Container from "../components/core/layout/Container";
+import AboutHero from "../components/about/AboutHero";
+import AboutLexica from "../components/about/AboutLexica";
+import AboutVisionMission from "../components/about/AboutVisionMission";
+import AboutPurpose from "../components/about/AboutPurpose";
+import AboutTeam from "../components/about/AboutTeam";
+import AboutFAQ from "../components/about/AboutFAQ";
 
 const AboutPage = () => {
     return (
         <>
-            <section className="relative bg-primary-600">
-                <section className="relative w-full h-[55dvh] bg-primary-600">
-                    <div className="absolute top-40 md:top-24 left-0 right-0 grid place-items-center">
-                        {/* <h1 className="text-white animate-bounce text-8xl">Lexica</h1> */}
-                    </div>
-                    <div className="absolute bottom-0 w-full h-[40vh]">
-                        <Image
-                            src='/images/about_hero.png'
-                            alt=""
-                            fill
-                            style={{
-                                objectFit: 'contain',
-                                objectPosition: 'center bottom'
-                            }}
-                        />
-                    </div>
-                </section>
-                <section className="w-full h-screen bg-white rounded-t-[6dvh] px-4 py-8">
-                    <Container>
-                        <section id="hero" className="flex flex-col items-center gap-4 md:gap-6">
-                            <h1 className="text-center w-4/5 md:w-3/5">Solusi dari permasalahan literasi Indoensia</h1>
-                            <pre className="whitespace-pre-wrap text-justify">
-                                Lexica merupakan aplikasi web GRATIS yang bertujuan untuk meningkatkan kemampuan literasi seluruh pelajar Indonesia serta menjadikan aktivitas membaca sebagai kegiatan yang menyenangkan.
-                                Kami melakukan ini lewat penyediaan artikel berkualitas dan menarik dengan tingkat kesulitan bacaan yang dapat disesuaikan. Dengan ini, pembaca dapat membaca dan mendapat ilmu baru dengan memilih tingkat kesulitan teks yang sesuai dengan kemampuan mereka.
-                                Lexica dapat diakses dari mana saja, lewat perangkat apa saja, dan dengan kondisi internet apa saja. Kami bermaksud untuk membuka pintu literasi kepada seluruh pelajar Indonesia, demi kemajuan pendidikan bangsa.
-                            </pre>
-                        </section>
-                    </Container>
-                </section>
-            </section>
+            <AboutHero />
+            <AboutLexica />
+            <AboutVisionMission />
+            <AboutPurpose />
+            <AboutTeam />
+            <AboutFAQ />
         </>
     );
 }
