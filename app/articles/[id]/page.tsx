1584a942b65248dfb99d7ffbad557feb5fa201e4
AnggaPutraa <yananggaa@gmail.com>
2023-06-30 14:11:47 +0800

    refactor: article detail layout

--- app/articles/[id]/page.tsx
+++ app/articles/[id]/page.tsx
@@ -10,12 +10,12 @@ import { Articles } from '@/app/constants/article.constant';
 
 
 
-interface ArticleDetailsProps { 
-  
+interface ArticleDetailsProps {
+
 }
 
 const ArticleDetails = ({
-  
+
 }: ArticleDetailsProps) => {
   const params = useParams();
   const id = parseInt(params.id);
@@ -23,60 +23,116 @@ const ArticleDetails = ({
   const article = Articles.filter(art => art.id === id)[0];
 
   return (
-    <div className='h-full pt-16'>
-      <div className='relative w-full py-4 md:py-6'>
-        <Container expanded>
-          <div className='w-full'>
-            <div className='font-bold text-2xl md:text-3xl text-slate-700'>
-              {article.title}
-            </div>
-            
-            <div className='relative h-full max-h-16 md:max-h-18 flex items-center pt-4 md:pt-6'>
-              <div className='w-full md:w-2/3 flex flex-row items-center'>
-                <div className='relative h-12 w-12 md:h-14 md:w-14 rounded-full bg-primary-500 overflow-hidden flex flex-col justify-center'>
-                  <Image 
-                    src='/images/author.jpg' 
-                    alt=''
-                    fill
-                    style={{
-                      objectFit: 'cover',
-                    }}
-                    className='h-full w-full'
-                  />
-                </div>
-                <div className='pl-4'>
-                  <div className='text-slate-600 font-semibold text-md md:text-sm'>
-                    {article.source}
-                    {article.adapted && (
-                      <span className='pl-1 md:pl-1.5'>
-                        (Adapted by Lexica)
-                      </span>
-                    )}
+    <>
+      <section className='w-full h-[30dvh] bg-primary-600 flex flex-row items-center justify-center'>
+        <div className='w-full max-w-md'>
+          <div className='h-2' />
+          <h2 className='text-center text-white'>{article.title}</h2>
+        </div>
+      </section>
+      <section className='w-full h-full bg-primary-600'>
+        <div className='w-full bg-white p-4 md:p-8 rounded-t-[6dvh]'>
+          <Container>
+            <div className='w-full'>
+              <div className='relative h-full max-h-16 md:max-h-18 flex items-center pt-4 md:pt-6'>
+                <div className='w-full md:w-2/3 flex flex-row items-center'>
+                  <div className='relative h-12 w-12 md:h-14 md:w-14 rounded-full bg-primary-500 overflow-hidden flex flex-col justify-center'>
+                    <Image
+                      src='/images/author.jpg'
+                      alt=''
+                      fill
+                      style={{
+                        objectFit: 'cover',
+                      }}
+                      className='h-full w-full'
+                    />
                   </div>
-                  <div className='text-slate-500 font-medium text-sm md:text-xs'>
-                    {article.author}
-                  </div>
-                  <div className='text-slate-400 font-medium text-sm md:text-xs'>
-                    {article.year}
+                  <div className='pl-4'>
+                    <div className='text-slate-600 font-semibold text-md md:text-sm'>
+                      {article.source}
+                      {article.adapted && (
+                        <span className='pl-1 md:pl-1.5'>
+                          (Adapted by Lexica)
+                        </span>
+                      )}
+                    </div>
+                    <div className='text-slate-500 font-medium text-sm md:text-xs'>
+                      {article.author}
+                    </div>
+                    <div className='text-slate-400 font-medium text-sm md:text-xs'>
+                      {article.year}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className='pt-4 md:pt-6'>
-              <div className='text-slate-600 font-semibold'>
-                DROPDOWN
+              <div className='pt-4 md:pt-6'>
+                <div className='text-slate-600 font-semibold'>
+                  DROPDOWN
+                </div>
               </div>
             </div>
-          </div>
-          <div className='w-full pt-6 md:pt-8'>
-            <pre className='whitespace-pre-line'>
-              {article.content}
-            </pre>
-          </div>
-        </Container>
-      </div>
-    </div>
+            <div className='w-full pt-6 md:pt-8'>
+              <pre className='whitespace-pre-line text-justify'>
+                {article.content}
+              </pre>
+            </div>
+          </Container>
+        </div>
+      </section>
+    </>
   );
 };
 
 export default ArticleDetails;
+
+{/* <div className='relative w-full py-4 md:py-6'>
+<Container expanded>
+  <div className='w-full'>
+    <div className='font-bold text-2xl md:text-3xl text-slate-700'>
+      {article.title}
+    </div>
+    
+    <div className='relative h-full max-h-16 md:max-h-18 flex items-center pt-4 md:pt-6'>
+      <div className='w-full md:w-2/3 flex flex-row items-center'>
+        <div className='relative h-12 w-12 md:h-14 md:w-14 rounded-full bg-primary-500 overflow-hidden flex flex-col justify-center'>
+          <Image 
+            src='/images/author.jpg' 
+            alt=''
+            fill
+            style={{
+              objectFit: 'cover',
+            }}
+            className='h-full w-full'
+          />
+        </div>
+        <div className='pl-4'>
+          <div className='text-slate-600 font-semibold text-md md:text-sm'>
+            {article.source}
+            {article.adapted && (
+              <span className='pl-1 md:pl-1.5'>
+                (Adapted by Lexica)
+              </span>
+            )}
+          </div>
+          <div className='text-slate-500 font-medium text-sm md:text-xs'>
+            {article.author}
+          </div>
+          <div className='text-slate-400 font-medium text-sm md:text-xs'>
+            {article.year}
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className='pt-4 md:pt-6'>
+      <div className='text-slate-600 font-semibold'>
+        DROPDOWN
+      </div>
+    </div>
+  </div>
+  <div className='w-full pt-6 md:pt-8'>
+    <pre className='whitespace-pre-line'>
+      {article.content}
+    </pre>
+  </div>
+</Container>
+</div> */}
