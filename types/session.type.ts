21e9badbc4c1eb431fda28eefe08ab7ee640edb0
AnggaPutraa <yananggaa@gmail.com>
2023-07-10 19:54:36 +0800

    chore: prettier

--- types/session.type.ts
+++ types/session.type.ts
@@ -1,12 +1,11 @@
 export type Session = {
-    user?: User;
-    expires?: string;
-    access_token?: string;
-  }
-  
-  export type User = {
-    name?: string | null | undefined;
-    email?: string | null | undefined;
-    image?: string | null | undefined;
-  }
-  
+  user?: User;
+  expires?: string;
+  access_token?: string;
+}
+
+export type User = {
+  name?: string | null | undefined;
+  email?: string | null | undefined;
+  image?: string | null | undefined;
+}
