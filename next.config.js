3c841e8a8e6129e533f13ffb4bb7abd64f999c11
AnggaPutraa <yananggaa@gmail.com>
2023-09-11 21:10:36 +0700

    chore: unoptimized image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    unoptimized: true,
+    remotePatterns: [
+      {
+        hostname: "https",
+        pathname: "lh3.googleusercontent.com",
+      },
+    ],
   },
 };
 
